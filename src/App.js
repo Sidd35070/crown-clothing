34802588d5a2558d8ef7a7a3f54a093f0886d5a3
Siddharth Rai <sidd35070@gmail.com>
2021-05-06 18:31:44 +0530

    Header done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import { Homepage } from './pages/homepage';
 import Shop from './pages/shop.component';
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { Header } from './components/header/header.component';
 
 function App() {
   
   return (
-    <Router>
+    <div>
+      <Header />
+      <Switch>
       <Route exact path="/" component={Homepage} ></Route>
       <Route exact path="/shop" component={Shop} ></Route>
-    </Router>
+    </Switch>
+    </div>
+    
   );
 }
 
